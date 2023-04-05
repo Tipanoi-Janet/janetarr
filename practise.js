75acd4b2d813286a63c767ef5e372789b9c799ea
Tipanoi-Janet <tipanoijanet@gmail.com>
2023-04-05 18:18:31 +0300

    first commit

---
+++ practise.js
@@ -0,0 +1,69 @@
+//write a function that accepts an array of strings and console.log each element using for loop
+function pdetail(detailz) {
+  for (let i = 0; i < detailz.length; i++) {
+    console.log(detailz[i]);
+  }
+}
+let detailz = ["School", "Cup", "computer"];
+pdetail(detailz);
+
+//write a function that accepts an array of numbers and uses the forEach () method to console.log each number multiplied by 2
+function pdetail(nums) {
+  nums.array.forEach((element) => {
+    console.log(element * 2);
+  });
+}
+let nums = [2, 5, 7, 8, 9];
+pdetail(nums);
+
+//write a function that takes in an array of numbers and consolethe first 4 items multiplied by 8 and the last 2 added by 5 console the array with the new values
+function arr(nums){fifth
+let i = 0;
+while(i < nums.length)
+console .log(num[i]); i++
+let multiply = 8;
+while(8<num.length)
+console.log (multiply* =[i]);
+
+}
+let arr(nums)= [2,7,6,9,12,14]
+console.log ({multiply});
+
+
+
+//write a function that takes in the following array and use while loop to iterate and break when the item is equal to the fourth index
+let arrnum = [1,2,3,4,5,6,7,8,9]
+while (let i = 0; i <num .length; i++){
+  console.log(i);
+  console.log (aarNum[i])
+}
+while (let i = 0;< num .length; i++){
+  if(i===4)
+  break;
+
+}
+console.log({"arrnum";arrnum[i]});
+while (let i = 0;i < num.length; i++){
+  if (i===4)
+  continue;
+
+}
+console.log ("continue" :num[i]);
+
+//write a function that takes in an array of strings and use a continue statement when the item is not of the second index
+let fruits = ["Apple","Plump","Banana","Strawberries"]
+(let i = 0; <num.length; i++){
+  console.log(i);
+  console.log(fruits [i]);
+}
+for (let i = 0; i< num.length;i++){
+  if(i===2){
+    break;
+  }
+console.log({"fruits" :num[i]});
+for (let i = 0;i < nums.length; i++){
+  if(i===2)
+  continue
+}
+console.log ({"continue";fruits[i]});
+}
